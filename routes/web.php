86b66c5677f0014b0b3a6a24447201c8a6313857
hongdeliu <t108598004@ntut.org.tw>
2019-10-30 23:36:55 +0800

    resources and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,32 +9,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 /*Route::get('/', function () {
     return view('welcome');
 });*/
-
 Route::get('/', function () {
     return view('pages/home',['title'=>'- Home']);
 });
-
-
 Route::get('about', function () {
     return view('pages/about',['title'=>'- About me']);
 });
-
 Route::get('service', function () {
     return view('pages/service',['title'=>'- Service']);
 });
-
 Route::get('history', function () {
     return view('pages/history',['title'=>'- History']);
 });
-
+Route::get('contact', function () {
+    return view('pages/contact',['title'=>'- Contact']);
+});
+Route::get('joinmember', function () {
+    return view('pages/joinmember',['title'=>'- Joinmember']);
+});
 Route::get('vip', function () {
     return view('pages/vip',['title'=>'- VIP']);
 });
-
 Route::get('login', function () {
     return view('pages/login',['title'=>'- Login']);
 });
