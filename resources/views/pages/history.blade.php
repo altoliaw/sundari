374e9395cd25da61cd518f68392bfc5e76d8481f
hongdeliu <t108598004@ntut.org.tw>
2019-11-01 14:43:27 +0800

    history hyperlink

--- resources/views/pages/history.blade.php
+++ resources/views/pages/history.blade.php
@@ -9,7 +9,39 @@
       <p class="description-class">三生盟國際事業有限公司在臺灣美容產業已具近20年的經驗，
                                   於公元2000年整合轉型為自然療法之身、心、靈SPA業者，專門提供臺灣知名SPA連鎖店，
                                   自然療法系列產品、教育課程、專業諮詢…等業務範圍。市場遍及全亞洲並以專業、天然、人文享譽業界。</p>
+      
     </div>
+    <div class='col-md-12'>
+      <h3 class="brand-class">德國MBK</h3>
+      <hr>
+      </div>
+      <div class='col-md-4'>
+        <a href = "https://www.mbk-cosmetics.com/tw/cn/index.html?title=en/_extras/startseite.php/_extras/startseite"><img src="img/mbk.jpg" /></a>
+      </div>
+      <div class='col-md-8'>
+        {{-- <hr> --}}
+        <p class="description-class">我發現每當我細心觀察每個人的整體後，我才能把我自己放在客人的角度去揣摩並推薦最適合的療程與產品給予客人。
+        往往這個理念幫助我的客人們最有效果的成效。
+        從1969年開始,BK女士在她的美容教育生涯中持續地傳達這個理念. 
+        BK的教育著重於幫助每個芳療師找到對的手法與產品去推廣最有療效的療程並更著重於客製化理念。
+        MBK系列產品擁有活化與調理的效果去改善各種不同的皮膚、頭髮與體重等問題</p>
+      </div>
+
+    <div class='col-md-12'>
+        <h3 class="brand-class">『AYURVEDA』艾鄔維達</h3>
+        <hr>
+        </div>
+        <div class='col-md-4'>
+          <a href = "http://www.sundari.com.tw/3g_info_428.php"><img src="img/ayurveda.jpg" style="width:100%;"></a>
+        </div>
+        <div class='col-md-8'>
+          {{-- <hr> --}}
+          <p class="description-class">艾鄔維達是一部人類最古老的生命科學，是由一群印度先知以直覺心智啟發而來。
+            這部寶典已經被人們運用了幾千年的歷史直到現在仍占重要地位。
+            艾鄔維達對健康定義是身、心、靈三者是否和諧工作。
+            這將直接影響一個人健康與疾病及幸福與長壽的品質。簡言之艾鄔維達講究於生活方式與態度，依據不同節令和氣候有不同的生活、飲食節制、運動、性、睡眠等的依循準則。
+            艾鄔維達認為壓力是由於外部和內部的不協調造成的。這些都可以透過艾鄔維達的體質飲食法、第三眼油滴法(Shirodhara)、艾鄔維達傳統按摩(Abhayanga)、冥想等得到療癒。因此，艾鄔維達是一種提供幸福，健康和長壽的科學，不僅只是替代的醫學系統。</p>
+        </div>
 </div>
 </div>
 @stop
